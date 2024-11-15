cf11472dabcc7b241abc0729154f94dcee76c1c3
gj-wes <negativegary@gmail.com>
2024-11-15 14:14:56 +0000

    Nuxt config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  devtools: { enabled: true },
   modules: [
     '@nuxthq/ui', 
     '@vueuse/nuxt'
-  ]
+  ],
+
+  compatibilityDate: '2024-11-15'
 })
