e8f2b4d521ebc6c557aec44d62cb6c6f82ec5309
gj-wes <negativegary@gmail.com>
2025-02-12 19:58:47 +0000

    Fixed default styling for UButton

--- app.config.ts
+++ app.config.ts
@@ -2,9 +2,15 @@ export default defineAppConfig({
   ui: {
     primary: 'blue',
     button: {
-      base: "text-xl rounded px-4 text-center uppercase leading-8 border-none cursor-pointer justify-center disabled:[--tw-bg-opacity:0.8]",
+      base: "rounded px-4 py-2 text-center uppercase leading-8 border-none cursor-pointer justify-center disabled:[--tw-bg-opacity:0.8]",
       font: "font-normal",
       rounded: "rounded",
+      size: {
+        xxl: "text-lg",
+      },
+      default: {
+        size: "xxl",
+      }
     },
     formGroup: {
       label: {
