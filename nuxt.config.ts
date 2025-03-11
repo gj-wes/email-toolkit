db9669d2c60e32aff225c09a6a30dbcf0246e671
gj-wes <negativegary@gmail.com>
2025-03-11 09:23:49 +0000

    Upgrade to Nuxt 3.16

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', '@nuxt/ui'],
+  modules: ['@vueuse/nuxt', '@nuxt/ui', '@nuxtjs/tailwindcss'],
 
   compatibilityDate: '2024-11-15'
 })
